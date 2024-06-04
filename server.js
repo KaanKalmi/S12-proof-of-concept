a604bcea3bfe282492d13571e350c1b723677d32
KB15 <kaan.kalmioglu@hva.nl>
2024-06-04 12:26:09 +0200

    fixed serverjs

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+// imports
 import express from 'express'
 import fetchJson from './helpers/fetch-json.js'
 
-const app = express()
+// variabelen
+const app = express(),
+      mp3 = 'http://playerservices.streamtheworld.com/api/livestream-redirect/BNR_BUSINESS_BEATS.mp3',
+      article = 'https://api.mobile.bnr.nl/v1/articles';
 
+// opzet project
 app.set('view engine', 'ejs')
 app.set('views', './views')
 app.use(express.static('public'))
@@ -13,10 +18,8 @@ app.listen(app.get('port'), function () {
  console.log(`Application started on http://localhost:${app.get('port')}`)
 })
 
-app.get('/', req res) function{
- res.render('index', iets)
+// GET route naar dashboard
+app.get('/', function(req, res){
+ res.render('index')
  console.log('if you see this message the page loaded correctly')
-}
-
-const mp3 = http://playerservices.streamtheworld.com/api/livestream-redirect/BNR_BUSINESS_BEATS.mp3,
-      article = https://api.mobile.bnr.nl/v1/articles;
+});
